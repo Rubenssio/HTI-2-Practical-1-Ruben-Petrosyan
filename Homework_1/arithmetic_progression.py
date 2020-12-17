41d2cedc6f771175a48c282c9ff9b6126a7f2b70
Ruben Petrosyan <petrosyanruben@gmail.com>
2020-12-17 22:00:02 +0400

    Updated homework 1 and readme

--- Homework_1/arithmetic_progression.py
+++ Homework_1/arithmetic_progression.py
@@ -1,6 +1,7 @@
-n1 = int(input())
-n2 = int(input())
-N = int(input())
+print('\nCreate an ARITHMETIC PROGRESSION by entering the first two members of the sequence!')
+n1 = int(input('Please enter the first member of the progression: '))
+n2 = int(input('Please enter the second member of the progression: '))
+N = int(input('\nWhich member of this progression would you like to see?\nPlease enter the N: '))
 
 step = n2 - n1
 nN = n1 + (N - 1) * step
--- Homework_1/century_from_year.py
+++ Homework_1/century_from_year.py
@@ -1,6 +1,19 @@
-year = int(input())
+print('\nYou are about to define a year as a century!')
+print('You can enter a year between 1 and 2021')
+year = int(input('.\nPlease enter the year: '))
 
 if year % 100 > 0:
-    print(year // 100 + 1)
+    century = year // 100 + 1
 else:
-    print(year // 100)
+    century = year // 100
+
+if century == 1 or century == 21:
+    suffix = 'st'
+elif century == 2:
+    suffix = 'nd'
+elif century == 3:
+    suffix = 'rd'
+else:
+    suffix = 'th'
+
+print(f'Year {year} is the {century}{suffix} century.\n')
