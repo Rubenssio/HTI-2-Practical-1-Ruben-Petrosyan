da2b18c617ce7abf99ab3722b99321f193ee4fd7
Ruben Petrosyan <petrosyanruben@gmail.com>
2020-12-21 20:03:00 +0400

    simplified the solution, added comments

--- Homework_1/century_from_year.py
+++ Homework_1/century_from_year.py
@@ -1,14 +1,25 @@
-print('\nYou are about to define "year" as a "century"!')
-print('You can enter a year between 1 and 2021')
-year = int(input('.\nPlease enter the year: '))
+# վերնագիր
+print('''
+You are about to define "year" as a "century"!
+You can enter a year between 1 and 2021
+''')
 
-if year % 100 > 0:
-    century = year // 100 + 1
-else:
-    century = year // 100
+# տարվա մուտքագրում user-ի կողմից
+year = int(input('Please enter the year: '))
 
+# ստուգում, արդյոք ներմուծված տարեթիվը բավարարում է խնդրի 1-2021 պահանջին
 if 0 < year < 2022:
 
+    # # ԳԼԽԱՎՈՐ ԿՈԴԸ՝ տարեթվից դարի ստացումը
+    century = round(year / 100 + 0.49)
+
+    # խնդիրը լուծելու այլ տարբերակ
+    """
+    import math
+    century = math.ceil(year / 100)
+    """
+
+    # որոշում ենք թե դարի վերջում ինչ վերջավորություն ենք տպելու
     if 3 < century < 21:
         suffix = 'th'
     elif century == 1 or century == 21:
@@ -18,7 +29,8 @@ if 0 < year < 2022:
     else:
         suffix = 'rd'
 
-    print(f'-ANSWER-\nYear {year} is the {century}{suffix} century.\n')
+    # արդյունքի տպում
+    print(f'\n-ANSWER-\nYear {year} is the {century}{suffix} century.\n')
 
-else:
-    print('-ANSWER-\nThe year entered is out of range (1-2021)\n')
+else:  # այն դեպքը երբ մուտքագրված թիվը 1-2021 միջակայքում չէ
+    print('\n-ANSWER-\nEntered year is out of range (1-2021)\n')
