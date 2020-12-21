6b35272b1976709a965b74fb73da453e3066677e
Ruben Petrosyan <petrosyanruben@gmail.com>
2020-12-21 20:14:16 +0400

    added comments

--- Homework_1/arithmetic_progression.py
+++ Homework_1/arithmetic_progression.py
@@ -1,10 +1,15 @@
+# վերնագիր
 print('\nCreate an ARITHMETIC PROGRESSION by entering the first two members of the sequence!')
+
+# տվյալների մուտքագրում user-ի կողմից
 n1 = int(input('Please enter the first member of the progression: '))
 n2 = int(input('Please enter the second member of the progression: '))
 N = int(input('\nWhich member of this progression would you like to see?\nPlease enter the N: '))
 
-step = n2 - n1
-nN = n1 + (N - 1) * step
+# ԳԼԽԱՎՈՐ ԿՈԴԸ
+step = n2 - n1  # գտնում ենք պրոգրեսիայի քայլը
+nN = n1 + (N - 1) * step  # գտնում ենք N-րդ անդամը
 
+# արդյունքի տպում
 print('\n-Answer-\nThe N-th member of this progression is:')
 print(f'[{N}] = {nN}\n')
