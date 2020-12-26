9ee46b52f5e588b786d6c4aefaa7e34a219f026a
Ruben Petrosyan <petrosyanruben@gmail.com>
2020-12-26 15:51:05 +0400

    removed unnecessary 'int's

--- Homework_2/is_lucky.py
+++ Homework_2/is_lucky.py
@@ -1,12 +1,13 @@
+print('is_lucky.py')
 lucky_num = input('Enter the number: ')
 
-first_sum = int()
-second_sum = int()
+first_sum = 0
+second_sum = 0
 
 for i in range(int(len(lucky_num) / 2)):
     first_sum += int(lucky_num[i])
 
-for i in range(int(len(lucky_num) / 2), int(len(lucky_num))):
+for i in range(int(len(lucky_num) / 2), len(lucky_num)):
     second_sum += int(lucky_num[i])
 
 if first_sum == second_sum:
