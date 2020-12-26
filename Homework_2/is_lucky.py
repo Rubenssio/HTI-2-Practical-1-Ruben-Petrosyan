8e57d7b7cb4fbd473c9d668da27b301e0c625c9e
Ruben Petrosyan <petrosyanruben@gmail.com>
2020-12-26 16:03:15 +0400

    replaced two loops with one loop

--- Homework_2/is_lucky.py
+++ Homework_2/is_lucky.py
@@ -4,11 +4,12 @@ lucky_num = input('Enter the number: ')
 first_sum = 0
 second_sum = 0
 
-for i in range(int(len(lucky_num) / 2)):
-    first_sum += int(lucky_num[i])
+for i in range(len(lucky_num)):
 
-for i in range(int(len(lucky_num) / 2), len(lucky_num)):
-    second_sum += int(lucky_num[i])
+    if i < len(lucky_num) / 2:
+        first_sum += int(lucky_num[i])
+    else:
+        second_sum += int(lucky_num[i])
 
 if first_sum == second_sum:
     print('Yes')
