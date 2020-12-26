18f26e2af7c9b43c6fd44639df848f9672b264e1
Ruben Petrosyan <petrosyanruben@gmail.com>
2020-12-27 00:10:26 +0400

    added ability to enter a number multiple times, improved UI

--- Homework_2/is_lucky.py
+++ Homework_2/is_lucky.py
@@ -1,29 +1,35 @@
 # վերնագիր
 print('''
+
 --- IS LUCKY ---
 This program will check whether the entered number is LUCKY
-(whether the sum of the first half digits of the number
- is equal to the sum of the digits of the second half)
+(whether the sum of the digits in the first half of the number
+ is equal to the sum of the digits in the second half)
 ''')
 
 # տվյալի մուտքագրում
-lucky_num = input('Please enter a number with even number of digits: \n')
+lucky_num = input('Please enter a number with even number of digits, or "q" to quit: \n')
 
-# հայտարարում ենք փոփոխականները
-first_sum = 0
-second_sum = 0
+while lucky_num != 'q':
 
-# անցնում ենք թվանշանի բոլոր նիշերով
-for i in range(len(lucky_num)):
+    # միաժամանակ հայտարարում և զրոյացնում ենք փոփոխականները
+    first_sum = 0
+    second_sum = 0
 
-    # գտնում ենք թե թվանշանի որ կեսում է գտնվում տվյալ նիշը
-    if i < len(lucky_num) / 2:
-        first_sum += int(lucky_num[i])
+    # անցնում ենք թվանշանի բոլոր նիշերով
+    for i in range(len(lucky_num)):
+
+        # գտնում ենք թե թվանշանի որ կեսում է գտնվում տվյալ նիշը
+        if i < len(lucky_num) / 2:
+            first_sum += int(lucky_num[i])  # ավելացնում ենք առաջին կեսի գումարին
+        else:
+            second_sum += int(lucky_num[i])  # կամ ավելիացնում ենք երկրորդ կեսի գումարին
+
+    # արդյյունքի ստուգում և տպում
+    print('\n    -ANSWER-')
+    if first_sum == second_sum:
+        print(f'    YES! The number {lucky_num} is LUCKY\n')
     else:
-        second_sum += int(lucky_num[i])
+        print(f'    No, the number {lucky_num} is NOT LUCKY\n')
 
-# արդյյունքի ստուգում և տպում
-if first_sum == second_sum:
-    print(f'\nYES! The number {lucky_num} is LUCKY\n')
-else:
-    print(f'\nNo, the number {lucky_num} is NOT LUCKY\n')
+    lucky_num = input('If you want to try again, enter a new number with even number of digits, or "q" to quit: \n')
