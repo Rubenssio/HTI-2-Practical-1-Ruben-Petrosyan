a03c9867a5fd7bd8d8f0eef86291634de7292b76
Ruben Petrosyan <petrosyanruben@gmail.com>
2020-12-27 16:12:49 +0400

    shortened the name from 'max_product' to 'MP'

--- Homework_2/adjacent_elements_product.py
+++ Homework_2/adjacent_elements_product.py
@@ -17,6 +17,7 @@ enter = 'numbers separated with spaces, or "q" to quit: \n'
 N = input(f'Please enter the {enter}')
 
 # երկու թվի արտադրյալ հաշվելու համար։ P-ն product բառից
+# անտեսում եմ code checker-ի՝ lambda-ն def դարձնելու պահանջը
 P = lambda x, y: x * y
 
 # ստուգում ենք արդյոք «q» է մուտքագրվել, թե՝ ոչ
@@ -25,16 +26,16 @@ while N != 'q':
     # մուտքագրված թվերը str տիպից դարձնում ենք int-եր պարունակող list
     N = [int(num) for num in N.split()]
 
-    # ենթադրում ենք, որ առաջին երկու անդամի արտադրյալը ամենամեծն է
-    max_product = N[0] * N[1]
+    # ենթադրում ենք, որ առաջին երկու անդամի արտադրյալը ամենամեծն է, MP = Max_Product
+    MP = N[0] * N[1]
     n1 = [N[0], 1]
     n2 = [N[1], 2]
 
     # ստուգում ենք՝ արդյոք կա ավելի մեծ արտադրյալով իրար կողք գտնվող երկու էլեմենտ
     for i in range(1, len(N) - 1):
 
-        if P(N[i],N[i + 1]) > max_product:
-            max_product = P(N[i],N[i + 1])
+        if P(N[i], N[i + 1]) > MP:
+            MP = P(N[i], N[i + 1])
 
             # հիշում ենք տվյալ թվերը և դրանց տեղը շարքում
             n1 = [N[i], i + 1]
@@ -44,7 +45,7 @@ while N != 'q':
     answer_counter += 1
     print(f'''
     -ANSWER N{answer_counter}-
-    The biggest product is: {max_product}
+    The biggest product is: {MP}
     
     it's the product of numbers {n1[0]} and {n2[0]}
     which are on positions [{n1[1]}] and [{n2[1]}]
