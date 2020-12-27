1ff7671d6698f0dc0f162730842e0b416f86959e
Ruben Petrosyan <petrosyanruben@gmail.com>
2020-12-27 12:37:13 +0400

    created UI

--- Homework_2/is_palindrome.py
+++ Homework_2/is_palindrome.py
@@ -1,10 +1,16 @@
-string = input('Enter text: ')
+print('''
+--- IS PALINDROME ---
+This program checks whether the entered
+text is a palindrome or not
+''')
 
-is_palindrome = 'Yes'
+string = input('Enter text: \n')
+
+is_palindrome = f'YES! "{string}" is a palindrome'
 
 for i in range(int(len(string) / 2)):
     if string[i] != string[- (i + 1)]:
-        is_palindrome = 'No'
+        is_palindrome = f'No, "{string}" is NOT a palindrome'
         break
 
-print(is_palindrome)
+print(f'\n-ANSWER-\n{is_palindrome}\n')
