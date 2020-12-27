a6a69c8b49a45cc8712d0c2f2a87e5efbf3ac313
Ruben Petrosyan <petrosyanruben@gmail.com>
2020-12-27 15:44:22 +0400

    shortened the name from 'numbers' to 'N'

--- Homework_2/adjacent_elements_product.py
+++ Homework_2/adjacent_elements_product.py
@@ -14,29 +14,29 @@ enter = 'numbers separated with spaces, or "q" to quit: \n'
 
 # տվյալների մուտքագրում
 # ակնկալում ենք բացատներով առանձնացված թվեր կամ «q»՝ ծրագրից դուրս գալու համար
-numbers = input(f'Please enter the {enter}')
+N = input(f'Please enter the {enter}')
 
 # ստուգում ենք արդյոք «q» է մուտքագրվել, թե՝ ոչ
-while numbers != 'q':
+while N != 'q':
 
     # մուտքագրված թվերը str տիպից դարձնում ենք int-եր պարունակող list
-    numbers = [int(num) for num in numbers.split()]
+    N = [int(num) for num in N.split()]
 
     # ենթադրում ենք, որ առաջին երկու անդամի արտադրյալը ամենամեծն է
-    max_product = numbers[0] * numbers[1]
-    n1 = [numbers[0], 1]
-    n2 = [numbers[1], 2]
+    max_product = N[0] * N[1]
+    n1 = [N[0], 1]
+    n2 = [N[1], 2]
 
     # ստուգում ենք՝ արդյոք կա ավելի մեծ արտադրյալով իրար կողք գտնվող երկու էլեմենտ
-    for i in range(1, len(numbers) - 1):
+    for i in range(1, len(N) - 1):
 
-        product = numbers[i] * numbers[i + 1]
+        product = N[i] * N[i + 1]
         if product > max_product:
             max_product = product
 
             # հիշում ենք տվյալ թվերը և դրանց տեղը շարքում
-            n1 = [numbers[i], i + 1]
-            n2 = [numbers[i + 1], i + 2]
+            n1 = [N[i], i + 1]
+            n2 = [N[i + 1], i + 2]
 
     # տպում ենք արդյունքը
     answer_counter += 1
@@ -49,4 +49,4 @@ while numbers != 'q':
     ''')
 
     # եթե օգտատերը ուզենա, կարող է նոր թվեր մուտքագրել կամ սեղմել «q»` ծրագրից դուրս գալու համար
-    numbers = input(f'If you want to try again, enter new {enter}')
+    N = input(f'If you want to try again, enter new {enter}')
