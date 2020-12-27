aff1469448131c4c3f16c23703c872df652c5a39
Ruben Petrosyan <petrosyanruben@gmail.com>
2020-12-27 12:54:52 +0400

    added ability to run the program multiple times

--- Homework_2/is_palindrome.py
+++ Homework_2/is_palindrome.py
@@ -1,16 +1,27 @@
+# վերնագիր
 print('''
 --- IS PALINDROME ---
 This program checks whether the entered
 text is a palindrome or not
 ''')
 
-string = input('Enter text: \n')
+# տվյալի մուտքագրում
+string = input('Enter text to check, or "-1" to exit the program: \n')
 
-is_palindrome = f'YES! "{string}" is a palindrome'
+# ծրագիրը աշխատացնում ենք քանի դեռ «-1» չի մուտքագրվել
+while string != "-1":
 
-for i in range(int(len(string) / 2)):
-    if string[i] != string[- (i + 1)]:
-        is_palindrome = f'No, "{string}" is NOT a palindrome'
-        break
+    # սկզբում ենթադրում ենք որ մուտքագրված տեքստը պալինդրոմ է
+    is_palindrome = f'YES! "{string}" is a palindrome'
 
-print(f'\n-ANSWER-\n{is_palindrome}\n')
+    # ստուգում ենք արդյոք մուտքագրված տեքստը իրականում պալինդրոմ է թե՝ ոչ
+    for i in range(int(len(string) / 2)):
+        if string[i] != string[- (i + 1)]:
+            is_palindrome = f'No, "{string}" is NOT a palindrome'
+            break
+
+    # տպում ենք արդյունքը
+    print(f'\n-ANSWER-\n{is_palindrome}\n')
+
+    # օգտատերին հնարավորություն ենք տալիս նոր տեքստ մուտքագրել ստուգելու համար
+    string = input('If you want to try again enter new text to check, or "-1" to exit the program: \n')
