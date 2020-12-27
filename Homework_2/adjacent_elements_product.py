334a692803d68d71b4a13665e5de4594e8425bef
Ruben Petrosyan <petrosyanruben@gmail.com>
2020-12-28 02:20:17 +0400

    removed lambda, refined the code

--- Homework_2/adjacent_elements_product.py
+++ Homework_2/adjacent_elements_product.py
@@ -14,29 +14,26 @@ enter = 'numbers separated with spaces, or "q" to quit: \n'
 
 # տվյալների մուտքագրում
 # ակնկալում ենք բացատներով առանձնացված թվեր կամ «q»՝ ծրագրից դուրս գալու համար
-N = input(f'Please enter the {enter}')
-
-# երկու թվի արտադրյալ հաշվելու համար։ P-ն product բառից
-# անտեսում եմ code checker-ի՝ lambda-ն def դարձնելու պահանջը
-P = lambda x, y: x * y
+Num = input(f'Please enter the {enter}')
 
 # ստուգում ենք արդյոք «q» է մուտքագրվել, թե՝ ոչ
-while N != 'q':
+while Num != 'q':
 
     # մուտքագրված թվերը str տիպից դարձնում ենք int-եր պարունակող list
-    N = [int(num) for num in N.split()]
+    Num = [int(n) for n in Num.split()]
 
     # ենթադրում ենք, որ առաջին երկու անդամի արտադրյալը ամենամեծն է
-    MP = N[0] * N[1]  # MP = Max_Product
-    s = 0  # s = the position of the first element from MP
+    MP = Num[0] * Num[1]    # առաջին երկու անդամի արտադրյալը
+    s = 0                   # առաջին անդամի դիրքը
 
     # ստուգում ենք՝ արդյոք կա ավելի մեծ արտադրյալով իրար կողք գտնվող երկու էլեմենտ
-    for i in range(1, len(N) - 1):
+    for i in range(1, len(Num) - 1):
 
-        if P(N[i], N[i + 1]) > MP:
-            MP = P(N[i], N[i + 1])
+        # եթե կա, վերագրում ենք այն MP-ին
+        if Num[i] * Num[i + 1] > MP:
+            MP = Num[i] * Num[i + 1]
 
-            # հիշում ենք տվյալ թվի տեղը շարքում
+            # հիշում ենք տվյալ անդամի տեղը շարքում
             s = i
 
     # տպում ենք արդյունքը
@@ -46,9 +43,12 @@ while N != 'q':
     -ANSWER N{answer_counter}-
     The biggest product is: {MP}
     
-    *The product of: {N[s]} and {N[s + 1]}
+    *The product of: {Num[s]} and {Num[s + 1]}
     *At positions: [{s + 1}] and [{s + 2}]
     ''')
 
     # եթե օգտատերը ուզենա, կարող է նոր թվեր մուտքագրել կամ սեղմել «q»` ծրագրից դուրս գալու համար
-    N = input(f'If you want to try again, enter new {enter}')
+    Num = input(f'If you want to try again, enter new {enter}')
+
+# Goodbye note
+print('Thanks for using our program!')
