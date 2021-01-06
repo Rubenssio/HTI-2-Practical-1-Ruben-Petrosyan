6dd0a07a6d297d086e9c4cc9d400e8e6301fecb6
Ruben Petrosyan <petrosyanruben@gmail.com>
2021-01-06 16:56:08 +0400

    added ability of finding the biggest product at multiple positions

--- Homework_2/adjacent_elements_product.py
+++ Homework_2/adjacent_elements_product.py
@@ -24,17 +24,22 @@ while Num != 'q':
 
     # ենթադրում ենք, որ առաջին երկու անդամի արտադրյալը ամենամեծն է
     MP = Num[0] * Num[1]    # առաջին երկու անդամի արտադրյալը
-    s = 0                   # առաջին անդամի դիրքը
+    s = [0]                   # առաջին անդամի դիրքը
 
     # ստուգում ենք՝ արդյոք կա ավելի մեծ արտադրյալով իրար կողք գտնվող երկու էլեմենտ
     for i in range(1, len(Num) - 1):
+        P = Num[i] * Num[i + 1]
 
         # եթե կա, վերագրում ենք այն MP-ին
-        if Num[i] * Num[i + 1] > MP:
-            MP = Num[i] * Num[i + 1]
+        if P > MP:
+            MP = P
 
             # հիշում ենք տվյալ անդամի տեղը շարքում
-            s = i
+            s = [i]
+
+        # նաև գտնում ենք նույն արտադրյալը ունեցող այլ թվեր
+        elif P == MP:
+            s.append(i)  # ու հիշում ենք դրանց տեղերը
 
     # տպում ենք արդյունքը
     answer_counter += 1
@@ -43,12 +48,14 @@ while Num != 'q':
     -ANSWER N{answer_counter}-
     The biggest product is: {MP}
     
-    *The product of: {Num[s]} and {Num[s + 1]}
-    *At positions: [{s + 1}] and [{s + 2}]
-    ''')
+    * The product of:''')
+
+    # նաև տպում ենք բոլոր այն դիրքերը, որտեղ գտել ենք ամենամեծ արտադրյալը
+    for i in s:
+        print(f'      {Num[i]} and {Num[i + 1]} at positions: [{i + 1}] and [{i + 2}]')
 
     # եթե օգտատերը ուզենա, կարող է մուտքագրել նոր թվեր կամ «q»` ծրագրից դուրս գալու համար
-    Num = input(f'If you want to try again, enter new {enter}')
+    Num = input(f'\nIf you want to try again, enter new {enter}')
 
 # Goodbye note
 print('Thanks for using our program!')
