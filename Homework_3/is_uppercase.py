fda60da5cbb5dc8c3d63c397cb8daa5610ece23a
Ruben Petrosyan <petrosyanruben@gmail.com>
2021-01-13 20:01:21 +0400

    basic working code (before the assignment)

---
+++ Homework_3/is_uppercase.py
@@ -0,0 +1,9 @@
+# այս պահի դրությամբ այս տնայինը դեռ չի հանձնարարվել, բայց արդեն հասանելի է Google Drive-ում
+
+def is_uppercase(text):
+    return text.isupper()
+
+if is_uppercase((input("Enter the text: "))):
+    print('Yes')
+else:
+    print('No')
