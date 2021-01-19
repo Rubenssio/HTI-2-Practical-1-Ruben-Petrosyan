a1b3edda167c23987bbea60ea8cc45e23e1fbd92
Ruben Petrosyan <petrosyanruben@gmail.com>
2021-01-19 20:04:10 +0400

    implemented recursion

--- Homework_3/number_root.py
+++ Homework_3/number_root.py
@@ -1,17 +1,17 @@
 def number_root(num):
     """Adds the digits of the given number,
-    then adoes the same thing to the result,
+    then does the same thing to the result,
     and so on until the result is a single digit number.
     Returns the single digit number"""
 
-    while num // 10 != 0:
-        root = 0
-        while num != 0:
-            root += num % 10
-            num //= 10
-        num, root = root, 0
+    if num <= 9:
+        return num
 
-    return num
+    root = 0
+    while num != 0:
+        root += num % 10
+        num //= 10
+    return number_root(root)
 
 
 print(number_root(int(input("Enter the number: "))))
