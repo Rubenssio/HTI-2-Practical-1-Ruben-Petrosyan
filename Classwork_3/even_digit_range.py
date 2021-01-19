3d0610ea2a16f116a305f7a815fe5b91aeca1271
Ruben Petrosyan <petrosyanruben@gmail.com>
2021-01-20 02:53:22 +0400

    Initial working code

---
+++ Classwork_3/even_digit_range.py
@@ -0,0 +1,21 @@
+def has_only_even_digit(num):
+    for i in str(num):
+        if int(i) % 2 == 0:
+            return False
+    return True
+
+
+def even_digit_range(start, stop):
+    result = []
+
+    for i in range(start, stop):
+        if has_only_even_digit(i):
+            result.append(i)
+
+    return result
+
+
+start_stop = [int(n) for n in input('Enter range in format "a-b": ').split('-')]
+
+print(even_digit_range(start_stop[0], start_stop[1]))
+# print(has_only_even_digit(int(input('enter: '))))
