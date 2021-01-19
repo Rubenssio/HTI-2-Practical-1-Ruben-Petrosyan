e07b3514c77898d513323f209d4ffaf978786102
Ruben Petrosyan <petrosyanruben@gmail.com>
2021-01-19 16:18:18 +0400

    added Documentation

--- Homework_3/is_prime.py
+++ Homework_3/is_prime.py
@@ -1,4 +1,9 @@
 def is_prime(num):
+    """Checks whether the given number is a prime number
+    and returns 'Yes' or 'No' accordingly:
+        'Yes' - if the given number IS prime,
+        'No'  - if the given number IS NOT prime"""
+
     for i in range(2, num):
         if num % i == 0:
             return 'No'
@@ -6,3 +11,10 @@ def is_prime(num):
 
 
 print(is_prime(int(input("Enter the number: "))))
+
+"""
+Wikipedia-ում պարզ թիվ ստուգելու Python կոդի շատ հետաքրքիր տարբերակ կա,
+ու ոնց հասկանում եմ՝ շատ էֆեկտիվ աշխատող
+https://en.wikipedia.org/wiki/Primality_test#Python_Code
+չնայած ֆունկցիան հայտարարելուց «-> bool» գրառումը դեռ չեմ հասկանում
+"""
