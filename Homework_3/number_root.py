c34a26ceedf5bed6987b3fe3708bb384aa759c83
Ruben Petrosyan <petrosyanruben@gmail.com>
2021-01-19 16:59:05 +0400

    added Documentation

--- Homework_3/number_root.py
+++ Homework_3/number_root.py
@@ -1,4 +1,9 @@
 def number_root(num):
+    """Adds the digits of the given number,
+    then adoes the same thing to the result,
+    and so on until the result is a single digit number.
+    Returns the single digit number"""
+
     while num // 10 != 0:
         root = 0
         while num != 0:
