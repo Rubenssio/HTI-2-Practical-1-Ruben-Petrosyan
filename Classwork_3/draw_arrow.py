a3054f493827d0fe64d1a0c988f1a20b351f5f7f
Ruben Petrosyan <petrosyanruben@gmail.com>
2021-01-19 23:21:05 +0400

    added another solution (which is using only one 'for' loop ) in comments

--- Classwork_3/draw_arrow.py
+++ Classwork_3/draw_arrow.py
@@ -5,6 +5,10 @@ def draw_arrow(n):
         print('* ' * (n - i))
 
     # for i in range(n * 2 - 1):
+    #     if i < n:
+    #         print('* ' * (i + 1))
+    #     else:
+    #         print('* ' * (n * 2 - i - 1))
 
 
 num = int(input('enter a positive number: '))
