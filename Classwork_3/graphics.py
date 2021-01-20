ac64182d38ae4f346a54fba77c85757a5bb02c27
Ruben Petrosyan <petrosyanruben@gmail.com>
2021-01-20 21:20:20 +0400

    Simplified the code

--- Classwork_3/graphics.py
+++ Classwork_3/graphics.py
@@ -1,36 +1,14 @@
 def draw_arrow(n, *, symbol='*', direction='right'):
 
-    if direction == 'right':
+    if direction == 'right' or 'left':
         # solution N3
         increment = 1
         i = 1
         while i != 0:
-            print(f'{symbol} ' * i)
-
-            if i == n:
-                increment = -1
-
-            i += increment
-
-        # solution N1
-        # for i in range(1, n + 1):
-        #     print(f'{symbol} ' * i)
-        # for i in range(1, n):
-        #     print(f'{symbol} ' * (n - i))
-
-        # solution N2
-        # for i in range(n * 2 - 1):
-        #     if i < n:
-        #         print(f'{symbol} ' * (i + 1))
-        #     else:
-        #         print(f'{symbol} ' * (n * 2 - i - 1))
-
-    if direction == 'left':
-
-        increment = 1
-        i = 1
-        while i != 0:
-            print('  ' * (n - i) + f' {symbol}' * i)
+            if direction == 'right':
+                print(f'{symbol} ' * i)
+            if direction == 'left':
+                print('  ' * (n - i) + f' {symbol}' * i)
 
             if i == n:
                 increment = -1
