8192d1ef0e659c667fca3880ff5498f592960517
Ruben Petrosyan <petrosyanruben@gmail.com>
2021-01-20 13:55:01 +0400

    Added a 'left' direction

--- Classwork_3/graphics.py
+++ Classwork_3/graphics.py
@@ -1,34 +1,54 @@
-def draw_arrow(n, symbol='*'):
+def draw_arrow(n, *, symbol='*', direction='right'):
 
-    # solution N3
-    increment = 1
-    i = 1
-    while i != 0:
-        print(f'{symbol} ' * i)
+    if direction == 'right':
+        # solution N3
+        increment = 1
+        i = 1
+        while i != 0:
+            print(f'{symbol} ' * i)
 
-        if i == n:
-            increment = -1
+            if i == n:
+                increment = -1
 
-        i += increment
+            i += increment
 
-    # solution N1
-    # for i in range(1, n + 1):
-    #     print(f'{symbol} ' * i)
-    # for i in range(1, n):
-    #     print(f'{symbol} ' * (n - i))
+        # solution N1
+        # for i in range(1, n + 1):
+        #     print(f'{symbol} ' * i)
+        # for i in range(1, n):
+        #     print(f'{symbol} ' * (n - i))
 
-    # solution N2
-    # for i in range(n * 2 - 1):
-    #     if i < n:
-    #         print(f'{symbol} ' * (i + 1))
-    #     else:
-    #         print(f'{symbol} ' * (n * 2 - i - 1))
+        # solution N2
+        # for i in range(n * 2 - 1):
+        #     if i < n:
+        #         print(f'{symbol} ' * (i + 1))
+        #     else:
+        #         print(f'{symbol} ' * (n * 2 - i - 1))
+
+    if direction == 'left':
+
+        increment = 1
+        i = 1
+        while i != 0:
+            print('  ' * (n - i) + f' {symbol}' * i)
+
+            if i == n:
+                increment = -1
+
+            i += increment
+
+    if direction == 'up':
+        print('\nThe "up" direction is not supported yet')
+
+    if direction == 'down':
+        print(('\nThe "down" direction is not supported yet'))
 
 
 num = int(input('enter a positive number: '))
-sign = input('enter the symbol or press enter to use the default: ')
+symb = input('enter the symbol or press "Enter" to use the default: ')
+dir = input('enter "right", "left", "up" or "down" for the direction: ')
 
-if sign == '':
-    draw_arrow(num)
+if symb == '':
+    draw_arrow(num, direction=dir)
 else:
-    draw_arrow(num, sign)
+    draw_arrow(num, symbol=symb, direction=dir)
