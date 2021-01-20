3849024e3b059d49d05cd610859887d59e9efd49
Ruben Petrosyan <petrosyanruben@gmail.com>
2021-01-20 20:17:32 +0400

    ---

--- Classwork_4/missing_number.py
+++ Classwork_4/missing_number.py
@@ -1,12 +1,1 @@
-def stools(height_list):
-    tallest = max(height_list)
-    sum = 0
-    for el in height_list:
-        sum += tallest - el
-
-    return sum
-
-
-numbers = [int(n) for n in input("Enter the numbers: ").split()]
-
-print(stools(numbers))
+# ուրիշ ֆայլի կոդն էի ստեղ գրել
