753852e438d0ec25e261701bf45b8d9fd9cf08c4
Ruben Petrosyan <petrosyanruben@gmail.com>
2021-01-22 18:55:52 +0400

    Solved the problem without using 'math' module

--- Homework_4/rectangle_perimeter.py
+++ Homework_4/rectangle_perimeter.py
@@ -1,7 +1,5 @@
 def segment_length(x1, y1, x2, y2):
-    import math  # ակնկալում եմ այս ֆունկցիան իրականացնել առանց math import անելու
-
-    return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+    return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** (1 / 2)
 
 
 def rectangle_perimeter(points1):
