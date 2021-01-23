077b6f231fa333e35ad79e2028d403e4c036ea25
Ruben Petrosyan <petrosyanruben@gmail.com>
2021-01-23 15:05:47 +0400

    Little refinements

--- Classwork_4/merge_sort.py
+++ Classwork_4/merge_sort.py
@@ -1,21 +1,20 @@
 def merge(l1, l2):
-    len1 = len(l1)
-    len2 = len(l2)
     i = j = 0
 
-    l_merge = []
+    l_merged = []
 
-    while i < len1 and j < len2:
+    while i < len(l1) and j < len(l2):
         if l1[i] < l2[j]:
-            l_merge.append(l1[i])
+            l_merged.append(l1[i])
             i += 1
         else:
-            l_merge.append(l2[j])
+            l_merged.append(l2[j])
             j += 1
 
-    l_merge.extend(l1[i:] + l2[j:])
+    l_merged.extend(l1[i:] + l2[j:])
+    # l_merged + l1[i:] + l2[j:]
 
-    return l_merge
+    return l_merged
 
 
 list1 = [int(n) for n in input('List 1: ').split()]
