32d0b400110a30040e0114484f45aa17fe6476e0
Ruben Petrosyan <petrosyanruben@gmail.com>
2021-01-25 17:26:01 +0400

    removed an unnecessary check 'if number == 1'

--- Homework_3/factorial.py
+++ Homework_3/factorial.py
@@ -1,6 +1,6 @@
 def factorial(number):
     """Returns the factorial of the given number"""
-    if number == 0 or number == 1:
+    if number == 0:
         return 1
     return number * factorial(number - 1)
 
