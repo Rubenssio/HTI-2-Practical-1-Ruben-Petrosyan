e3048fb7d9843a58c59a5639fbf943dd28ab2672
Ruben Petrosyan <petrosyanruben@gmail.com>
2021-01-26 19:47:33 +0400

    got rid of 'append()'

--- Classwork_5/reverse_list_tail.py
+++ Classwork_5/reverse_list_tail.py
@@ -1,8 +1,7 @@
 def reverse_list_tail(l, l_rev=[]):
     if not l:
         return l_rev
-    l_rev.append(l[-1])
-    return reverse_list_tail(l[:-1], l_rev)
+    return reverse_list_tail(l[:-1], l_rev + [l[-1]])
 
 
 print(reverse_list_tail([4, 3, 2, 1]))
