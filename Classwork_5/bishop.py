7ac5f1517fe4983ecf2b0f3116c1f131432a1a0a
Ruben Petrosyan <petrosyanruben@gmail.com>
2021-01-27 13:22:29 +0400

    Turned diagonal matrix transformation into a function

--- Classwork_5/bishop.py
+++ Classwork_5/bishop.py
@@ -18,27 +18,28 @@ def output_matrix(mtx):
         print()
 
 
-#MAIN PROGRAM
+def transform_diagonal(mat, direction, B_loc):
+    for d in direction:
+        i = B_loc[0]
+        j = B_loc[1]
+        while 0 <= i < len(mat) and 0 <= j < len(mat):
+            matrix[i][j] = 'x'
+            i += d[0]
+            j += d[1]
+
+    matrix[B_location[0]][B_location[1]] = 'B'
+
+
+# MAIN PROGRAM
 n_of_rows = int(input('Number of rows: '))
 
 matrix, B_location = input_matrix_and_find(n_of_rows, 'B')
-
-num_of_rows = len(matrix)
-num_of_cols = len(matrix[0])
 
 print('B is at', B_location)
 
 if type(B_location) is not str:
     directions = [(1, 1), (1, -1), (-1, 1), (-1, -1)]
 
-    for dir in directions:
-        i = B_location[0]
-        j = B_location[1]
-        while 0 <= i < num_of_rows and 0 <= j < num_of_cols:
-            matrix[i][j] = 'x'
-            i += dir[0]
-            j += dir[1]
+    transform_diagonal(matrix, directions, B_location)
 
-    matrix[B_location[0]][B_location[1]] = 'B'
-
-    output_matrix(matrix)
+output_matrix(matrix)
