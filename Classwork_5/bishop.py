4f86ac259ba017ac12b9f454d1d3134b52a0d590
Ruben Petrosyan <petrosyanruben@gmail.com>
2021-01-27 12:22:23 +0400

    Working code with room for omptimization

--- Classwork_5/bishop.py
+++ Classwork_5/bishop.py
@@ -25,9 +25,47 @@ num_of_cols = len(matrix[0])
 
 B_location = 'unknown location'
 
+found = False
 for i in range(num_of_rows):
     for j in range(num_of_cols):
         if matrix[i][j] == 'B':
             B_location = (i, j)
+            found = True
+            break
+    if found:
+        break
 
 print('B is at', B_location)
+
+i = B_location[0]
+j = B_location[1]
+while i >= 0 and j < num_of_cols:
+    matrix[i][j] = 'x'
+    i -= 1
+    j += 1
+
+i = B_location[0]
+j = B_location[1]
+while i < num_of_rows and j < num_of_cols:
+    matrix[i][j] = 'x'
+    i += 1
+    j += 1
+
+i = B_location[0]
+j = B_location[1]
+while i < num_of_rows and j >= 0:
+    matrix[i][j] = 'x'
+    print(i, j)
+    i += 1
+    j -= 1
+
+i = B_location[0]
+j = B_location[1]
+while i >= 0 and j >= 0:
+    matrix[i][j] = 'x'
+    i -= 1
+    j -= 1
+
+matrix[B_location[0]][B_location[1]] = 'B'
+
+output_matrix(matrix)
