c1e659fb0e08fc46b18cf93cdcf2f3a933419d2d
Ruben Petrosyan <petrosyanruben@gmail.com>
2021-01-27 12:44:45 +0400

    minor fixes

--- Classwork_5/bishop.py
+++ Classwork_5/bishop.py
@@ -1,7 +1,7 @@
 def input_matrix(n):
     mat = []
     for _ in range(n):
-        mat.append([el for el in input().split()])
+        mat.append(input().split())
     return mat
 
 
@@ -14,17 +14,15 @@ def output_matrix(mtx):
         print()
 
 
+# MAIN PROGRAM
 n_of_rows = int(input('Number of rows: '))
 
 matrix = input_matrix(n_of_rows)
-
-# output_matrix(matrix)
 
 num_of_rows = len(matrix)
 num_of_cols = len(matrix[0])
 
 B_location = 'unknown location'
-
 found = False
 for i in range(num_of_rows):
     for j in range(num_of_cols):
@@ -37,16 +35,17 @@ for i in range(num_of_rows):
 
 print('B is at', B_location)
 
-directions = [(1, 1), (1, -1), (-1, 1), (-1, -1)]
+if type(B_location) is not str:
+    directions = [(1, 1), (1, -1), (-1, 1), (-1, -1)]
 
-for dir in directions:
-    i = B_location[0]
-    j = B_location[1]
-    while 0 <= i < num_of_rows and 0 <= j < num_of_cols:
-        matrix[i][j] = 'x'
-        i += dir[0]
-        j += dir[1]
+    for dir in directions:
+        i = B_location[0]
+        j = B_location[1]
+        while 0 <= i < num_of_rows and 0 <= j < num_of_cols:
+            matrix[i][j] = 'x'
+            i += dir[0]
+            j += dir[1]
 
-matrix[B_location[0]][B_location[1]] = 'B'
+    matrix[B_location[0]][B_location[1]] = 'B'
 
-output_matrix(matrix)
+    output_matrix(matrix)
