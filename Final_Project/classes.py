a565d5df271fac44dd9eed41d1ae6933e79f1c3c
Ruben Petrosyan <petrosyanruben@gmail.com>
2021-03-17 08:18:36 +0400

    Added docstrings

--- Final_Project/classes.py
+++ Final_Project/classes.py
@@ -3,7 +3,40 @@ import re
 
 
 class Laptop:
+    """Consists of the basic knowledge about the laptop.
+
+    Attributes
+    ----------
+    raw_screen_size : float
+        The screen size in inches without any formatting.
+    screen_size : str
+        The screen size with a '"' sign at the end.
+    raw_price : float
+        The price in euros without any formatting.
+    price : str
+        The price in euros with '€' sign at the end,
+        and with a comma for the decimal point.
+    """
+
     def __init__(self, manufacturer, model, screen_size, price):
+        """Gets the basic parameters of a laptop.
+
+        Parameters
+        ----------
+        manufacturer : str
+            The brand name of the laptop.
+        model : str
+            The model of the laptop.
+        screen_size : int, float, str
+            The diagonal size of the screen in inches,
+            stored as float.
+        price : int, float, str
+            The price of the laptop in euros,
+            must be a number without additional characters e.g. currency symbol,
+            the decimal point can be both '.' and ',' ,
+            stored as float.
+        """
+
         self.manufacturer = manufacturer
         self.model = model
 
@@ -15,10 +48,14 @@ class Laptop:
 
     @property
     def raw_screen_size(self):
+        """float: the screen size in inches without any formatting."""
         return self._screen_size
 
     @property
     def screen_size(self):
+        """str: the screen size with a '"' sign at the end.
+        The setter can receive either a string with or without '"' at the end,
+        or int/float."""
         return f'{self._screen_size}"'
 
     @screen_size.setter
@@ -35,10 +72,15 @@ class Laptop:
 
     @property
     def raw_price(self):
+        """float: the price in euros without any formatting."""
         return self._price
 
     @property
     def price(self):
+        """str: the price with '€' sign at the end,
+        and with a comma for the decimal point.
+        The setter must receive a number without additional characters e.g. currency symbol,
+        the decimal point can be both '.' and ',' ."""
         value = f'{self._price:.2f}€'.replace('.', ',')
         return value
 
@@ -53,12 +95,67 @@ class Laptop:
                 raise ValueError('Price must be entered in euros as a number (without letters or characters.')
 
     def __repr__(self):
+
         return f'{self.manufacturer} {self.model} {self.screen_size} {self.price}'
 
 
 class LaptopWithSpecs(Laptop):
+    """Consists of the basic knowledge and the specifications of the laptop.
+
+    Attributes
+    ----------
+    raw_ram : int
+        The size of the RAM in bytes.
+    ram : str
+        The size of the RAM in a human readable format,
+        with a 'B' or 'KB' or 'MB' etc. at the end,
+        rounded to the nearest 0.1.
+    raw_weight : float
+        The weight of the laptop in kg without any formatting.
+    weight : str
+        The weight of the laptop in kg with 'kg' at the end.
+    """
+
     def __init__(self, manufacturer, model, screen_size, price,
                  category, screen, cpu, ram, storage, gpu, the_os, os_version, weight):
+        """Consists of the specifications in addition to basic parameters of a laptop.
+
+        Parameters
+        ----------
+        manufacturer : str
+            The brand name of the laptop.
+        model : str
+            The model of the laptop.
+        screen_size : int, float, str
+            The diagonal size of the screen in inches,
+            stored as float.
+        price : int, float, str
+            The price of the laptop in euros,
+            must be a number without additional characters e.g. currency symbol,
+            the decimal point can be both '.' and ',' .
+            stored as float
+        category : str
+            The category of the laptop e.g. 'Ultrabook'.
+        screen : str
+            The screen technology, resolution etc.
+        cpu : str
+            The specifications of the CPU.
+        ram : str
+            The size of the RAM of the laptop,
+            with 'B', 'KB', 'MB' or etc. at the end.
+        storage : str
+            The storage of the laptop.
+        gpu : str
+            The specifications of the GPU.
+        the_os : str
+            The OS of the laptop.
+        os_version : str
+            The version of the OS.
+        weight : str
+            The weight of the laptop in grams or kilograms,
+            'g' or 'k' must follow the weight value accordingly.
+        """
+
         super().__init__(manufacturer, model, screen_size, price)
         self.category = category
         self.screen = screen
@@ -77,6 +174,20 @@ class LaptopWithSpecs(Laptop):
 
     @staticmethod
     def byte_to_human(byte):
+        """Gets a memory size in bytes and returns human readable version.
+
+        Parameters
+        ----------
+        byte : int
+            The memory size in bytes.
+
+        Returns
+        -------
+        str
+            Human readable version of the memory size, with 'B', 'KB', 'MB' or etc. at the end.
+
+        """
+
         p = int(math.log(byte, 1024))
 
         value = byte / 1024 ** p  # getting more human readable version
@@ -89,10 +200,14 @@ class LaptopWithSpecs(Laptop):
 
     @property
     def raw_ram(self):
+        """int: the size of the RAM in bytes without any formatting."""
         return self._ram
 
     @property
     def ram(self):
+        """str: the size of the RAM in a readable way,
+        with 'B', 'KB', 'MB' or etc. at the end.
+        The setter converts readable version into bytes"""
         return self.byte_to_human(self._ram)
 
     @ram.setter
@@ -127,10 +242,15 @@ class LaptopWithSpecs(Laptop):
 
     @property
     def raw_weight(self):
+        """float: the weight of the laptop in kg without any formatting."""
         return self._weight
 
     @property
     def weight(self):
+        """str: the weight of the laptop in kg with 'kg' at the end.
+        The setter can receive the value both in grams an kilograms,
+        'g' or 'k' must follow the value accordingly."""
+
         unit = 'kg'
 
         if self._weight < 1:
